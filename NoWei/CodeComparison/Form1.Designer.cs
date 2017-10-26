948ee5db7311dee5d595bc3c6180cfde17d99470
jsc2412697426 <2412697426@qq.com>
2017-10-26 18:37:09 +0800

    JSC

---
+++ NoWei/CodeComparison/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace CodeComparison
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioDown = new System.Windows.Forms.RadioButton();
+            this.radioUp = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_CodeMacth = new System.Windows.Forms.Button();
+            this.Btn_Exits = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labdot5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioDown);
+            this.groupBox1.Controls.Add(this.radioUp);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(14, 24);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(423, 78);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "按键测试区";
+            // 
+            // radioDown
+            // 
+            this.radioDown.AutoSize = true;
+            this.radioDown.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioDown.Location = new System.Drawing.Point(270, 28);
+            this.radioDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.radioDown.Name = "radioDown";
+            this.radioDown.Size = new System.Drawing.Size(62, 21);
+            this.radioDown.TabIndex = 1;
+            this.radioDown.TabStop = true;
+            this.radioDown.Text = "下翻页";
+            this.radioDown.UseVisualStyleBackColor = true;
+            // 
+            // radioUp
+            // 
+            this.radioUp.AutoSize = true;
+            this.radioUp.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioUp.Location = new System.Drawing.Point(55, 28);
+            this.radioUp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.radioUp.Name = "radioUp";
+            this.radioUp.Size = new System.Drawing.Size(62, 21);
+            this.radioUp.TabIndex = 1;
+            this.radioUp.TabStop = true;
+            this.radioUp.Text = "上翻页";
+            this.radioUp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioUp.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "对码步骤：";
+            // 
+            // Btn_CodeMacth
+            // 
+            this.Btn_CodeMacth.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_CodeMacth.Location = new System.Drawing.Point(95, 117);
+            this.Btn_CodeMacth.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Btn_CodeMacth.Name = "Btn_CodeMacth";
+            this.Btn_CodeMacth.Size = new System.Drawing.Size(87, 33);
+            this.Btn_CodeMacth.TabIndex = 1;
+            this.Btn_CodeMacth.Text = "对码";
+            this.Btn_CodeMacth.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Exits
+            // 
+            this.Btn_Exits.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_Exits.Location = new System.Drawing.Point(236, 117);
+            this.Btn_Exits.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Btn_Exits.Name = "Btn_Exits";
+            this.Btn_Exits.Size = new System.Drawing.Size(87, 33);
+            this.Btn_Exits.TabIndex = 1;
+            this.Btn_Exits.Text = "退出";
+            this.Btn_Exits.UseVisualStyleBackColor = true;
+            this.Btn_Exits.Click += new System.EventHandler(this.Btn_Exits_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.labdot5);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(14, 168);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(423, 232);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.Location = new System.Drawing.Point(37, 204);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(351, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "*有指示灯的接收器，第3步指示灯会长亮，第5步会熄灭变闪烁。";
+            // 
+            // labdot5
+            // 
+            this.labdot5.AutoSize = true;
+            this.labdot5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labdot5.Location = new System.Drawing.Point(42, 165);
+            this.labdot5.Name = "labdot5";
+            this.labdot5.Size = new System.Drawing.Size(53, 17);
+            this.labdot5.TabIndex = 0;
+            this.labdot5.Text = "labdot5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(42, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(303, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "4、反复按翻页激光笔的上、下翻页键给接收器发信号。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(42, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(267, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "3、点击上方对码按钮，使接收器进入对码状态。";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(43, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "2、打开翻页激光笔的电源开关。";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(43, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "1、将接收器插入电脑的USB口。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(32, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(379, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Knorvay诺为 翻页激光笔领导品牌，2011-2013连续3年全网销量领先";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(163, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "www.knorvay.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 458);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Btn_Exits);
+            this.Controls.Add(this.Btn_CodeMacth);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Knorvay诺为 翻页激光笔对码软件 V1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioUp;
+        private System.Windows.Forms.Button Btn_CodeMacth;
+        private System.Windows.Forms.Button Btn_Exits;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labdot5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ NoWei/CodeComparison/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeComparison
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioUp.Checked = false;
+            radioDown.Checked = false;
+            labdot5.Text = string.Format(@"5、接收器收到对码信号，保存发射器的编码，再按上下翻页键时，
+    上方的按键测试区会有变化，表示对码成功。");
+            
+        }
+
+        private void Btn_Exits_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+            System.Environment.Exit(System.Environment.ExitCode);
+            
+        }
+    }
+}
